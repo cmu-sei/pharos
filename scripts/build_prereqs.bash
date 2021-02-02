dc610900041add46074f78571c83ec1249506c73
Michael Welsh Duggan <mwd@cert.org>
2021-02-01 21:45:03 -0500

    Update Rose version in docker script.

--- scripts/build_prereqs.bash
+++ scripts/build_prereqs.bash
@@ -50,7 +50,7 @@ test "$1" = "-reclaim" && rm -rf $DIR/z3
 cd $DIR
 test -d rose && rm -rf rose
 
-git clone --depth 1 -b v0.10.7.16 https://github.com/rose-compiler/rose rose
+git clone --depth 1 -b v0.11.11.2 https://github.com/rose-compiler/rose rose
 cd rose
 
 # See rose issue #52
